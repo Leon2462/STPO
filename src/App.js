3864bb1c809d7997d267cf3d166ad03183887549
Leon2462 <saintmupip@gmail.com>
2021-03-25 00:59:45 +0300

    add id's for html elements

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ const App = () => {
     }
     return (
         <div>
-            <strong>Вариант №9</strong>
-            <p>
+            <strong id ='header'>Вариант №9</strong>
+            <p id ='task'>
                 Ввести массив не более чем из 10 трехзначных целых чисел. Удалить из
                 массива те числа, которые удовлетворяют условию: число является простым и
                 если к этому числу прибавить 1, то полученное число будет являться степенью
@@ -47,12 +47,12 @@ const App = () => {
                 заданному условию: оно простое, 31+1=25 и 5 простое. Вывести измененный
                 массив или сообщение о том, что нет чисел, удовлетворяющих условию.
             </p>
-            <label>Поле для ввода элементов массива: </label>
-             <input style = {{width: '300px'}}type = "text" onChange = {inputHandler} value = {inputValue}/>
-            <button style = {{margin: '10px'}} onClick={clearInput}>Очистить поле</button>
-            <button onClick={deleteElem}>Удалить элементы</button>
-            <div style={{whiteSpace: 'pre-line', color: 'red' }}>{errors}</div>
-            <div>{result}</div>
+            <label id = 'label'>Поле для ввода элементов массива: </label>
+             <input id = 'input' style = {{width: '300px'}}type = "text" onChange = {inputHandler} value = {inputValue}/>
+            <button id = 'clearbtn' style = {{margin: '10px'}} onClick={clearInput}>Очистить поле</button>
+            <button id = 'deletebtn'onClick={deleteElem}>Удалить элементы</button>
+            <div id = 'errormessage' style={{whiteSpace: 'pre-line', color: 'red' }}>{errors}</div>
+            <div id = 'result' >{result}</div>
         </div>
     )
 }
